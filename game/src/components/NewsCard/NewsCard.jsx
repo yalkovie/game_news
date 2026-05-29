5ef12282f3c6d90a3108c324c261967ebcebd1ca
Ilya <yalkovie@mail.ru>
2026-05-29 20:00:54 +0300

    stile 2

--- game/src/components/NewsCard/NewsCard.jsx
+++ game/src/components/NewsCard/NewsCard.jsx
@@ -3,9 +3,9 @@ import './NewsCard.css'
 function NewsCard ({arcticle}) {
     return (
         <>
-        <div>
+        <div className='card'>
             <h1>{arcticle.title}</h1>
-            <img src={arcticle.image}/>
+            <img src={arcticle.image} className='main_img'/>
             <p>{arcticle.content}</p>
             <p>{arcticle.publicationDate}</p>
         </div>
